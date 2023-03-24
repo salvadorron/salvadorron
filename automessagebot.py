ebfd0b4a4066f09551964f245882ff51df07e1cd
Salvador Ron <salvadorronosio@gmail.com>
2023-03-23 22:03:15 -0400

    fix automessage bot

--- automessagebot.py
+++ automessagebot.py
@@ -1,16 +1,20 @@
+import asyncio
 import time
 import requests
+import telegram
 
 # configure details from chat id and telegram bot
 
 bot_token = '5715000920:AAFTrx_erER612yrXS-96CUGDZfMgxI70tY'
 bot_chatID = '1316829775'
-api_URL = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+bot = telegram.Bot(token=bot_token)
 
-# define a list to checkout urls
+async def sendMessage(msg):
+	await bot.send_message(chat_id=bot_chatID, text=msg)
 
-#urls = ['https://example.com','https://www.google.com','https://www.facebook.com']
-urls = ['http://localhost:4000/shapes']
+# define a list of urls to check
+
+urls = ['https://example.com','https://www.google.com','https://www.facebook.com']
 
 
 while True:
@@ -22,17 +26,19 @@ while True:
 			# if response isn't succesfully (http status code 200), send message to telegram
 			if response.status_code != 200:
 				message = f'La URL {url} no esta disponible'
-				requests.post(api_URL, json={'chat_id': bot_chatID, 'text': message})
+				asyncio.run(sendMessage(message))
 				print(f'Se ha enviado un mensaje de telegram debido a una falla en la URL {url}.')
 				
-			#waiting for 30 seconds before to resend another message
+			#wait 30 seconds to send another message
 			time.sleep(30)
 		
 	except:
 		print('No se pudo obtener acceso a la pagina')
 	finally:
-		#waiting 05 minutes before checkout to URLs again
+		#wait 05 minutes before checking urls again
 		time.sleep(300)
+
+
 
 
 
